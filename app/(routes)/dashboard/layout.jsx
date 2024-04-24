3ffed5249976c428faab7a57b27e95e4e6b381d0
Francis Iwu <apkfem@gmail.com>
2024-04-24 08:41:33 +0200

    creating the sidenav in the dashboard

--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import SideNav from "./_components/SideNav";
+import DashboardHeader from "./_components/DashboardHeader";
 
 const DashboardLayout = ({ children }) => {
   return (
     <div>
-      <div>
+      <div className="fixed md:w-64 hidden md:block">
         <SideNav />
       </div>
-      <div>{children}</div>
+      <div className="md:ml-64">
+        <DashboardHeader />
+        {children}
+      </div>
     </div>
   );
 };
