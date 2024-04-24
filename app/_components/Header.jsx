54138aceac0eb8c3efbc72b0c64fd56013d1cc24
Francis Iwu <apkfem@gmail.com>
2024-04-24 07:35:15 +0200

    installing and configuring Clerk for authentication, sign-in and sign-up

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,12 +1,23 @@
-import { Button } from "@/components/ui/button";
+//"use client";
+
+import { UserButton, useUser } from "@clerk/nextjs";
+import { Button } from "../../components/ui/button";
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
+  const { user, isSignedIn } = useUser;
   return (
     <div className="p-5 flex justify-between items-center border shadow-lg">
       <Image src={"./logo.svg"} alt="logo" width={160} height={100} />
-      <Button>Get Started</Button>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <Link href={"/sign-in"}>
+          <Button>Get Started</Button>
+        </Link>
+      )}
     </div>
   );
 };
